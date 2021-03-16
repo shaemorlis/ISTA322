549aa6d6ef6bf26448b30aef09427580b263ea43
shaemorlis <77212782+shaemorlis@users.noreply.github.com>
2021-03-15 21:01:57 -0400

    quizzes

---
+++ Quizzes/quiz7.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CSharpQuiz7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is CSharp Quiz 07.");
+            IFirearm Shotgun = new Shotgun();
+            Shotgun.Say("shotgun", "Boom");
+            IFirearm Rifle = new Rifle();
+            Rifle.Say("rifle", "Bang");
+            IFirearm Pistol = new Pistol();
+            Pistol.Say("pistol", "Pop");
+        }
+        interface IFirearm
+        {
+            void Say(string type, string action);
+        }
+        private class Shotgun : IFirearm
+        {
+            public void Say(string type, string action)
+            {
+                Console.WriteLine($"I am a {type} and I go {action}");
+            }
+        }
+        private class Rifle : IFirearm
+        {
+            public void Say(string type, string action)
+            {
+                Console.WriteLine($"I am a {type} and I go {action}");
+            }
+        }
+        private class Pistol : IFirearm
+        {
+            public void Say(string type, string action)
+            {
+                Console.WriteLine($"I am a {type} and I go {action}");
+            }
+        }
+    }
+}
---
+++ Quizzes/quiz9.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quiz_09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<String> myStrStack = new Stack<string>();
+            Queue<String> myStrQueue = new Queue<string>();
+            Dictionary<String, int> myDictionary = new Dictionary<string, int>();
+
+            myStrStack.Push("One");
+            myStrStack.Push("Two");
+            myStrStack.Push("Three");
+
+            myStrQueue.Enqueue("One");
+            myStrQueue.Enqueue("Two");
+            myStrQueue.Enqueue("Three");
+
+            myDictionary.Add("One",1);
+            myDictionary.Add("Two",2);
+            myDictionary.Add("Three",3);
+
+
+            Console.WriteLine("This is C Sharp quiz 9");
+
+            Console.WriteLine("Here is the stack");
+            foreach (string item in myStrStack)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Here is the queue");
+            foreach (string item in myStrQueue)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Here is the dictionary");
+            foreach (KeyValuePair<string,int> keyValPair in myDictionary)
+            {   
+                Console.WriteLine("{0} {1}", keyValPair.Key, keyValPair.Value);
+            }
+
+            Console.WriteLine("Press any key to continue.....");
+            Console.ReadKey();
+
+        }
+    }
+}
